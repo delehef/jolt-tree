71232a650be4a377c818f386286729c7d50f5345
Franklin Delehelle <franklin.delehelle@odena.eu>
2024-04-16 12:58:12 +0200

    make it work

--- guest/src/lib.rs
+++ guest/src/lib.rs
@@ -1,18 +1,10 @@
 #![cfg_attr(feature = "guest", no_std)]
 #![no_main]
 
-use buf_view::BufView;
-
-#[jolt::provable]
-fn tree_hash(serialized: &[u8]) -> Vec<u8> {
-    let mut buf_view = BufView::wrap(&serialized);
+#[jolt::provable(stack_size = 8_000_000, memory_size = 100_000_000)]
+fn tree_hash(serialized: &[u8]) -> [u8; 32] {
+    let mut buf_view = trivial_tree::buf_view::BufView::wrap(&serialized);
     let db = trivial_tree::Node::parse(&mut buf_view).unwrap();
-    // db.pretty();
-    // println!("done");
-
-    // println!("computing root hash...");
     let root_hash = db.hash();
-
-    // println!("done: {:x?}.", root_hash);
-    return root_hash;
+    return root_hash.try_into().unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 pub fn main() {
-    let serialized = trivial_tree::random_tree(10, 10);
-    let (prove_tree, verify_tree) = guest::build_tree_hash();
+    let serialized = trivial_tree::random_tree(1, 2);
+    let program_summary = guest::analyze_tree_hash(&serialized);
+    program_summary
+        .write_to_file("failing.txt".into())
+        .expect("should write");
 
-    let (output, proof) = prove_tree(&serialized);
-    let is_valid = verify_tree(proof);
-
-    println!("output--: {:?}", output);
-    println!("valid: {}", is_valid);
+    // let (prove_tree, verify_tree) = guest::build_tree_hash();
+    // let (output, proof) = prove_tree(&serialized);
+    // let is_valid = verify_tree(proof);
+    // println!("output--: {:?}", output);
+    // println!("valid: {}", is_valid);
 }
